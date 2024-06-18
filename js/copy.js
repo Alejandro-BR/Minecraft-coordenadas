f571cfeb7439cd55ef2eb5997cf9a22b46178a27
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2024-06-18 19:25:56 +0200

    Boton de copiar

---
+++ js/copy.js
@@ -0,0 +1,22 @@
+// Alejandro Barrionuevo Rosado
+
+/**
+ * Funcion que copia las coordenadas.
+ */
+function copyCoordinates() {
+
+  let xN, zN, yN, coordinates;
+
+  xN = parseFloat(document.calculadora.xNether.value);
+  zN = parseFloat(document.calculadora.zNether.value);
+  yN = parseFloat(document.calculadora.yNether.value);
+
+  coordinates = `${xN}, ${zN}, ${yN}`;
+
+  // Utilizar el Clipboard API para copiar al portapapeles
+  navigator.clipboard.writeText(coordinates).then(function () {
+    alert("Coordenadas copiadas: " + coordinates);
+  }).catch(function (err) {
+    console.error('Error al copiar al portapapeles: ', err);
+  });
+}
