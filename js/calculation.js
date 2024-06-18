b79b6ad6d769a455ea6c398246e2d8b7e2e5bd50
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2024-06-18 12:53:05 +0200

    Controlar los valores

--- js/calculation.js
+++ js/calculation.js
@@ -7,17 +7,27 @@
  */
 function calculate() {
   const DIVIDER = 8;
+  const MAX = 29999984; // Cordenada mas alta
+  const MIN = -29999984; // Cordenada mas baja
+  const HEIGHT_MAX = 320; // Limite de altura
+  const HEIGHT_MIN = -64; // Limite del suelo
+
   let xO, zO, yO, xN, zN, yN;
 
   // Obtiene las coordenadas del Overworld desde el formulario.
-  xO = parseInt(document.calculadora.xOverworld.value);
-  zO = parseInt(document.calculadora.zOverworld.value);
-  yO = parseInt(document.calculadora.yOverworld.value);
+  xO = parseFloat(document.calculadora.xOverworld.value);
+  zO = parseFloat(document.calculadora.zOverworld.value);
+  yO = parseFloat(document.calculadora.yOverworld.value);
 
-    // Verifica si las coordenadas son numeros validos.
+  // Verifica si las coordenadas son numeros validos.
   if (isNaN(xO) || isNaN(zO) || isNaN(yO)) {
-    console.error("Las coordenadas del Overworld deben ser numeros validos.");
-    alert("Por favor, ingrese coordenadas validas para el Overworld.");
+    error("Las coordenadas deben ser números.");
+    return;
+  } else if (!isInteger(xO) || !isInteger(zO) || !isInteger(yO)) {
+    error("Las coordenadas deben ser números enteros.");
+    return;
+  } else if (xO > MAX || xO < MIN || yO > MAX || yO < MIN || zO > HEIGHT_MAX || zO < HEIGHT_MIN) {
+    error("Las coordenadas deben estar dentro de los valores del juego.");
     return;
   }
 
@@ -41,4 +51,39 @@ function calculate() {
 function restart() {
   document.calculadora.reset();
   console.log("Formulario reiniciado.");
+}
+
+/**
+ * Comprueba si un numero es un entero:
+ * 
+ * true  --> si es un entero
+ * false --> si no
+ * 
+ * @param num int
+ * @returns boolean
+ * 
+ */
+function isInteger(num) {
+  return num % 1 === 0;
+}
+
+/**
+ * Genera un mensaje de error.
+ * @param {string} message - El mensaje de error a mostrar.
+ */
+function error(message) {
+  console.error(message);
+  alert(message);
+}
+
+/**
+ * Valida que el valor ingresado sea un numero entero.
+ * @param {HTMLInputElement} input - El elemento de entrada a validar.
+ */
+function validateIntegerInput(input) {
+  const value = input.value;
+  if (!isInteger(parseFloat(value))) {
+    input.value = value.slice(0, -1);
+    error("Las coordenadas deben ser números enteros.");
+  }
 }
